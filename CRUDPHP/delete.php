bed4dff955f7931ab1f69563bee8509ead0f932c
Eduard Roland Donor <88637382+ewardronin35@users.noreply.github.com>
2024-03-05 06:55:22 -0800

    Update delete.php

--- CRUDPHP/delete.php
+++ CRUDPHP/delete.php
@@ -1,13 +1,19 @@
 <?php
 // Include database connection
-require_once('database.php');
+require_once('Connection/dbconn.php');
 
 // Check if form submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST['id'];
 
-    // Delete user
-    $result = $user->delete($id);
-    echo $result;
+    // SQL delete query
+    $sql = "DELETE FROM getdatatable WHERE id = $id";
+
+    // Execute the SQL statement
+    if ($conn->query($sql) === TRUE) {
+        echo "Data Deleted Successfully";
+    } else {
+        echo "Error Deleting Data: " . $conn->error;
+    }
 }
 ?>
